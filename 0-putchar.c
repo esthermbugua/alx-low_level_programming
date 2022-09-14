48f380724c5a38e82c82494a8dc1370d36f5deb9
esthermbugua <mohshee8@gmail.com>
2022-09-14 01:25:38 -0700

    _putchar

---
+++ 0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing a string with _putchar
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int s = 0;
+	char h[] = "_putchar";
+
+	while (h[s])
+	{
+		putchar(h[s]);
+		s++;
+	}
+	putchar('\n');
+	return (0);
+}
